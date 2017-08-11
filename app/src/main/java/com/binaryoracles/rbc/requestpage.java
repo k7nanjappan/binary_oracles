311664645761b77d3941b0861bf7c5e7a846fd59
Kesavan Nanjappn <k7nanjappan@gmail.com>
2017-08-12 03:02:40 +0545

    Android commit 6

--- app/src/main/java/com/binaryoracles/rbc/requestpage.java
+++ app/src/main/java/com/binaryoracles/rbc/requestpage.java
@@ -2,6 +2,12 @@ package com.binaryoracles.rbc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+
 
 public class requestpage extends AppCompatActivity {
 
@@ -9,5 +15,15 @@ public class requestpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requestpage);
+        Spinner spinner = (Spinner) findViewById(R.id.bgroup);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.bgroup, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.urgency);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.urgency, android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
     }
 }
