48a23c9e679a7a4b6029d919743d30f87b49f9a2
B Rutvik <rutvikbrk@gmail.com>
2017-08-12 08:50:39 +0530

    added image and changes in html code

--- details.php
+++ details.php
@@ -1,12 +1,14 @@
 <?php
-if(isset($_POST['b1'])){
+if(isset($_POST['b3'])){
 	$name=$_POST['name'];
 	$pno=$_POST['pno'];
 	$locality=$_POST['locality'];
 	$group=$_POST['group'];
 	$weight=$_POST['weight'];
+	$uid=$_POST['uid'];
+	$pass=$_POST['pass'];
 	
-	$data=array("$name,$pno,$locality,$group,$weight");
+	$data=array("$name,$pno,$locality,$group,$weight,$uid,$pass");
 	$file=fopen("contacts.csv","a+");
 	foreach($data as $line)
 	{
@@ -18,14 +20,70 @@ if(isset($_POST['b1'])){
 		while(!feof($file_handle)){
 			$line_of_text[]=fgetcsv($file_handle,1024);
 		}
-		fclose($file_handle);
+		//fclose($file_handle);
 		return $line_of_text;
 		}
 		$csvFile='contacts.csv';
 		$csv=getdata($csvFile);
 		echo '<pre>';
-		($a=$csv[0]);
-		print $a[0];
-		echo '</pre>';
+		//$a=$csv[0];
+		//echo $a;
+		//$b=$csv[6];
+		}
+if(isset($_POST['b1'])){
+	$cuid=$_POST['cuid'];
+	$cpass=$_POST['cpass'];
+	$x=0;
+	$y=6;
+	$i=0;
+/*while(feof)
+{
+	$a=$csv[i][x];
+	$b=$csv[i][y];
+	if(strcmp($cuid,$a)==0)
+	{
+		if(strcmp($cpass,$b)==0)
+		{
+			window.open("donor.html");
+			break;
+		}
+		else
+		{
+			echo "invalid username or password";
+		}
 	}
+	else
+		{
+			echo "invalid username or password";
+		}
+		$x+=7;
+		$y+=6;
+		$i++;
+}*/
+while(feof($file_handle))
+{
+	$a=$csv[x];
+	$b=$csv[y];
+	if(strcmp($cuid,$a)==0)
+	{
+		if(strcmp($cpass,$b==0))
+		{
+			window.open("donor.html");
+			break;
+		}
+		else
+		{
+				echo "invalid user-id od password"; 
+		}
+	}
+	else
+	{
+		echo "invalid unsername or password";
+	}
+	$x+=7;
+	$y+=6;
+}
+
+fclose($file_handle);
+}
 ?>
--- try.php
+++
@@ -1,4 +0,0 @@
-<?php
-$a=array(file_get_contents("try.txt"));
-echo $a[1]
-?>
