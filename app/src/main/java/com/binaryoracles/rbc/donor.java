7d1e59cae83f46ce6f919fab07d25e86c4a22f22
Kesavan Nanjappn <k7nanjappan@gmail.com>
2017-08-12 11:14:26 +0545

    Android final 2

--- app/src/main/java/com/binaryoracles/rbc/donor.java
+++ app/src/main/java/com/binaryoracles/rbc/donor.java
@@ -1,9 +1,13 @@
 package com.binaryoracles.rbc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class donor extends AppCompatActivity {
 
@@ -16,5 +20,17 @@ public class donor extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.bgroup, android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        Button sub2;
+        sub2 = (Button) findViewById(R.id.sub2);
+        sub2.setOnClickListener(new View.OnClickListener() {
+            @Override
+                    public void onClick(View view)
+            {
+                 Toast.makeText(getApplicationContext(),"Thank you for joining RBC. Your contributions will be appreciated!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(donor.this, startview.class);
+                startActivity(intent);
+            }
+        });
     }
 }
